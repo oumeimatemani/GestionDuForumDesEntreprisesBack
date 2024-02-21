a917c03aa070238ba2110fee143279e02b2124b7
oumeima temani <oumeima.temani>
2024-02-21 15:17:44 +0100

    first commit

---
+++ src/test/java/tn/esprit/GestionDuForumDesEntreprises/GestionDuForumDesEntreprisesApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.GestionDuForumDesEntreprises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDuForumDesEntreprisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
